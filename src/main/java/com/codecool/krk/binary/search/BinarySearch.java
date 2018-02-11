cc842e145648ef3672390ac5d0e89451e1140388
Korges <korgesik@gmail.com>
2018-02-11 13:28:33 +0100

    Add BinarySearch Algoritm

---
+++ src/main/java/com/codecool/krk/binary/search/BinarySearch.java
@@ -0,0 +1,26 @@
+package com.codecool.krk.binary.search;
+
+public class BinarySearch {
+
+    public static boolean runBinarySearch(int[] numbersList, int searchedNumber) {
+
+        int first = 0;
+        int last = numbersList.length - 1;
+        int iteration = 0;
+
+        while(first<=last) {
+            iteration++;
+            int middle = (first + last)/2;
+
+            if(numbersList[middle] > searchedNumber) {
+                last = middle - 1 ;
+            } else if (numbersList[middle] < searchedNumber) {
+                first = middle + 1;
+            } else if (numbersList[middle] == searchedNumber) {
+//                System.out.println("Found at iteration number : " + iteration);
+                return true;
+            }
+        }
+        return false;
+    }
+}
