c9cea1af4db8aee28c621cb93dc38b1584bc0e3c
Korges <korgesik@gmail.com>
2018-02-11 15:16:05 +0100

    Add TinkerTailor Algorithm

---
+++ src/main/java/com/codecool/krk/tinker/tailor/TinkerTailor.java
@@ -0,0 +1,45 @@
+package com.codecool.krk.tinker.tailor;
+
+import java.util.ArrayList;
+
+public class TinkerTailor {
+
+    private int amountOfPlayers;
+    private int syllabe;
+    private int position;
+    ArrayList playersList = new ArrayList();
+
+    public TinkerTailor(int amountOfPlayers, int syllabe) {
+
+        this.amountOfPlayers = amountOfPlayers;
+        this.syllabe = syllabe;
+        this.position = syllabe;
+        playersList = fillPlayersList();
+    }
+
+    public ArrayList fillPlayersList() {
+
+        for(int i=1; i <= amountOfPlayers; i++) {
+            playersList.add(i);
+        }
+        return playersList;
+    }
+
+    public int letsPlay() {
+
+        System.out.println(playersList);
+
+        if(playersList.size() == 1) {
+            System.out.println("And the winner is: " + playersList.get(0));
+
+        } else {
+            playersList.remove(position-1);
+            position+=syllabe -1;
+            if(position>playersList.size()) {
+                position = position%playersList.size();
+            }
+            letsPlay();
+        }
+        return (int) playersList.get(0);
+    }
+}
